c69272a34796ade1dd3af4c97214d517856bef04
teaguea <ateague2014@gmail.com>
2021-12-14 02:47:06 -0500

    Enabled the SalesPage to display products that are on sale that the page retrieves from the database

---
+++ TRMDesktopUI.Library/Api/IProductEndpoint.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TRMDesktopUI.Library.Model;
+
+namespace TRMDesktopUI.Library.Api
+{
+    public interface IProductEndpoint
+    {
+        Task<List<ProductModel>> GetAll();
+    }
+}
