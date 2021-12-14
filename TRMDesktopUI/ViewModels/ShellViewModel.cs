b516fb00f3db9aa4fbb64184d8221c8ce767900d
teaguea <ateague2014@gmail.com>
2021-12-14 01:09:48 -0500

    Added Event Aggregation to the WPF Project which will now allow the user to login and then be taken to the Sales Page

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -4,17 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using TRMDesktopUI.EventModels;
 
 namespace TRMDesktopUI.ViewModels
 {
-    public class ShellViewModel : Conductor<object>
+    public class ShellViewModel : Conductor<object>, IHandle<LogOnEvent>
     {
-        private LoginViewModel _loginVM;
+        private IEventAggregator _events;
+        private SalesViewModel _salesVM;
+        SimpleContainer _container;
 
-        public ShellViewModel(LoginViewModel loginVM)
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM,
+            SimpleContainer container)
         {
-            _loginVM = loginVM;
-            ActivateItem(_loginVM);
+            _events = events;
+            _events.Subscribe(this);
+            _salesVM = salesVM;
+            _container = container;
+
+
+            ActivateItem(_container.GetInstance<LoginViewModel>());
+        }
+
+        public void Handle(LogOnEvent message)
+        {
+            ActivateItem(_salesVM);
         }
     }
 }
