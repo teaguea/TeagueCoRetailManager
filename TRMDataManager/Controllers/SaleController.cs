333b9cbaa5398e86a6b0e901553538ab33c16549
teaguea <ateague2014@gmail.com>
2021-12-15 22:51:26 -0500

    Added Roles to the API

--- TRMDataManager/Controllers/SaleController.cs
+++ TRMDataManager/Controllers/SaleController.cs
@@ -13,6 +13,7 @@ namespace TRMDataManager.Controllers
     [Authorize]
     public class SaleController : ApiController
     {
+        [Authorize(Roles = "Cashier")]
         public void Post(SaleModel sale)
         {
             SaleData data = new SaleData();
@@ -21,6 +22,7 @@ namespace TRMDataManager.Controllers
             data.SaveSale(sale, userId);
         }
 
+        [Authorize(Roles = "Admin,Manager")]
         [Route("GetSalesReport")]
         public List<SaleReportModel> GetSaleReports()
         {
