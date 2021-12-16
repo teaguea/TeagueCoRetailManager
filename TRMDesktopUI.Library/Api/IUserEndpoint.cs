b52d88e00f289f2ea7847d3b9703474d792b9382
teaguea <ateague2014@gmail.com>
2021-12-16 01:59:13 -0500

    Add WPF to display user and their role(s)

---
+++ TRMDesktopUI.Library/Api/IUserEndpoint.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TRMDesktopUI.Library.Model;
+
+namespace TRMDesktopUI.Library.Api
+{
+    public interface IUserEndpoint
+    {
+        Task<List<UserModel>> GetAll();
+    }
+}
