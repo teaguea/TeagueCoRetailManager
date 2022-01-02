c1c51cf8ad5e239f5a8e8df0bd8fecd4beaccf30
teaguea <ateague2014@gmail.com>
2022-01-02 00:59:37 -0500

    Added appsetting Config File

--- Portal/Authentication/AuthStateProvider.cs
+++ Portal/Authentication/AuthStateProvider.cs
@@ -1,5 +1,6 @@
 ﻿using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,19 +15,24 @@ namespace Portal.Authentication
     {
         private readonly HttpClient _httpClient;
         private readonly ILocalStorageService _localStorage;
+        private readonly IConfiguration _config;
         private readonly AuthenticationState _anonymous;
 
-        public AuthStateProvider(HttpClient httpClient, ILocalStorageService localStorage)
+        public AuthStateProvider(HttpClient httpClient,
+                                 ILocalStorageService localStorage,
+                                 IConfiguration config)
         {
             _httpClient = httpClient;
             _localStorage = localStorage;
+            _config = config;
             _anonymous = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
         }
 
 
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            var token = await _localStorage.GetItemAsync<string>("authToken");
+            string authTokenStorageKey = _config["authTokenStorageKey"];
+            var token = await _localStorage.GetItemAsync<string>(authTokenStorageKey);
 
             if (string.IsNullOrWhiteSpace(token))
             {
